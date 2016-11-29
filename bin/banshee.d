01d547508e42d5e2baa62b62df5f38e00e179f37
Daniel Koch <xuedi@beijingcode.org>
2016-11-29 17:37:10 +0100

    cleaned up a bit

--- bin/banshee.d
+++ bin/banshee.d
@@ -3,26 +3,25 @@
 import std.stdio;
 import std.file;
 import std.process;
-import core.sys.posix.stdlib;
 import core.thread;
 
-void main() {
+int main() {
 	write("Starting Banshee ");
 	if(exists("/home/xuedi/private/myMusic/empty")) {
 		pipeShell("sshfs -o nonempty pollux:/home/xuedi/private/myMusic /home/xuedi/private/myMusic");
-		for( int a=10; a<20; a =a+1 ) {
+		foreach(i; 0 .. 10) {
 			write("-");
 			if(!exists("/home/xuedi/private/myMusic/empty")) {
 				pipeShell("banshee");
 				writeln("> OK");
-				exit(-1);
+				return 0;
 			}
 			Thread.sleep(1.seconds);
 		}
 		writeln("--> FAILED: Could no mount!");
-		exit(1);
+		return 1;
 	}
 	writeln("--> OK");
 	pipeShell("banshee");
-	exit(-1);
+	return 0;
 }
