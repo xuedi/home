36b40e20d4aa81335bb0f4f9c6bf762e90b9414b
Daniel Koch <xuedi@beijingcode.org>
2017-01-21 16:19:28 +0100

    clean up

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -198,17 +198,13 @@ xmenu = {
   },
   { "vm", {
     { "-virtualbox-", "virtualbox" },
-    { "FnortVM", "VBoxManage startvm 9f5eed8d-9988-40b1-9395-7f3d30499622" },
-    { "Gateway", "VBoxManage startvm a3f4999a-3690-4f13-af9c-1c6fe18361df --type headless" },
-    { "UbuntuBox", "VBoxManage startvm 79f15811-1242-4362-82df-c5dab765a090" },
-    { "WindowXP", "VBoxManage startvm 040e2ce1-47ec-4e48-b6ec-5ef448bd196d" }}
+    { "kitematic", "kitematic" }}
   },
   { "crypto", {
     { "zenmap", "sudo zenmap" },
     { "gpa (PGP)", "gpa" },
     { "vidalia", "vidalia" },
     { "zmap", "zmap" },
-    { "armory (btc)", "armory" },
     { "Seahorse", "seahorse" },
     { "cryptkeeper", "cryptkeeper" }}
   },
@@ -391,30 +387,12 @@ globalkeys = awful.util.table.join(
     -- Prompt
     awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
 
-    awful.key({ modkey }, "x",
-              function ()
-                  awful.prompt.run({ prompt = "Run Lua code: " },
-                  mypromptbox[mouse.screen].widget,
-                  awful.util.eval, nil,
-                  awful.util.getdir("cache") .. "/history_eval")
-              end),
     -- Menubar
     awful.key({ modkey }, "p", function() menubar.show() end)
 )
 
 clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
-    awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end),
-    awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
-    awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
-    awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
-    awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
-    awful.key({ modkey,           }, "n",
-        function (c)
-            -- The client currently has the input focus, so it cannot be
-            -- minimized, since minimized clients can't have the focus.
-            c.minimized = true
-        end),
     awful.key({ modkey,           }, "m",
         function (c)
             c.maximized_horizontal = not c.maximized_horizontal
