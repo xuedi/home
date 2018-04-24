58e44a329e1eae83f4638dda1f64463892404a01
xuedi <xuedi@beijingcode.org>
2018-04-24 17:39:09 +0200

    added paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,5 @@ alias netstat='sudo netstat -tulpn'
 alias 雪地='ponysay 雪地很厉害'
 alias steam='STEAM_RUNTIME=0 steam'
 
+set -gx PATH /home/xuedi/.config/composer/vendor/bin $PATH
+set -gx PATH /home/xuedi/go/bin/ $PATH
